df569cb257a8398df9531cdf841d9a81d2a229f4
Alan Xiong <AlanXiong2916886783@gmail.com>
2024-04-04 10:44:59 +0800

    去除回车，避免docker故障

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
 set -e
 set -x
-
 # Cleanup
 rm -rf /var/www/html/*
-
 # Copy frontend files
 cp /speedtest/*.js /var/www/html/
-
 # Copy favicon
 cp /speedtest/favicon.ico /var/www/html/
-
 # Set up backend side for standlone modes
 if [ "$MODE" == "standalone" ]; then
   cp -r /speedtest/backend/ /var/www/html/backend
@@ -19,30 +14,25 @@ if [ "$MODE" == "standalone" ]; then
     sed -i s/\$IPINFO_APIKEY\ =\ \'\'/\$IPINFO_APIKEY\ =\ \'$IPINFO_APIKEY\'/g /var/www/html/backend/getIP_ipInfo_apikey.php
   fi
 fi
-
 if [ "$MODE" == "backend" ]; then
   cp -r /speedtest/backend/* /var/www/html
   if [ ! -z "$IPINFO_APIKEY" ]; then
     sed -i s/\$IPINFO_APIKEY\ =\ \'\'/\$IPINFO_APIKEY\ =\ \'$IPINFO_APIKEY\'/g /var/www/html/getIP_ipInfo_apikey.php
   fi
 fi
-
 # Set up index.php for frontend-only or standalone modes
 if [ "$MODE" == "frontend" ]; then
   cp /speedtest/frontend.php /var/www/html/index.php
 elif [ "$MODE" == "standalone" ]; then
   cp /speedtest/standalone.php /var/www/html/index.php
 fi
-
 # Apply Telemetry settings when running in standalone or frontend mode and telemetry is enabled
 if [[ "$TELEMETRY" == "true" && ( "$MODE" == "frontend" || "$MODE" == "standalone" ) ]]; then
   cp -r /speedtest/results /var/www/html/results
-
   if [ "$MODE" == "frontend" ]; then
     mkdir /var/www/html/backend
     cp /speedtest/backend/getIP_util.php /var/www/html/backend
   fi
-
   if  [ "$DB_TYPE" == "mysql" ]; then
     sed -i 's/$db_type = '\''.*'\''/$db_type = '\'$DB_TYPE\''/g' /var/www/html/results/telemetry_settings.php
     sed -i 's/$MySql_username = '\''.*'\''/$MySql_username = '\'$DB_USERNAME\''/g' /var/www/html/results/telemetry_settings.php
@@ -61,31 +51,23 @@ if [[ "$TELEMETRY" == "true" && ( "$MODE" == "frontend" || "$MODE" == "standalon
   else
     sed -i s/\$db_type\ =\ \'.*\'/\$db_type\ =\ \'sqlite\'\/g /var/www/html/results/telemetry_settings.php
   fi
-
   sed -i s/\$Sqlite_db_file\ =\ \'.*\'/\$Sqlite_db_file=\'\\\/database\\\/db.sql\'/g /var/www/html/results/telemetry_settings.php
   sed -i s/\$stats_password\ =\ \'.*\'/\$stats_password\ =\ \'$PASSWORD\'/g /var/www/html/results/telemetry_settings.php
-
   if [ "$ENABLE_ID_OBFUSCATION" == "true" ]; then
     sed -i s/\$enable_id_obfuscation\ =\ .*\;/\$enable_id_obfuscation\ =\ true\;/g /var/www/html/results/telemetry_settings.php
   fi
-
   if [ "$REDACT_IP_ADDRESSES" == "true" ]; then
     sed -i s/\$redact_ip_addresses\ =\ .*\;/\$redact_ip_addresses\ =\ true\;/g /var/www/html/results/telemetry_settings.php
   fi
-
   mkdir -p /database/
   chown www-data /database/
 fi
-
 chown -R www-data /var/www/html/*
-
 # Allow selection of Apache port for network_mode: host
 if [ "$WEBPORT" != "80" ]; then
   sed -i "s/^Listen 80\$/Listen $WEBPORT/g" /etc/apache2/ports.conf
   sed -i "s/*:80>/*:$WEBPORT>/g" /etc/apache2/sites-available/000-default.conf
 fi
-
 echo "Done, Starting APACHE"
-
 # This runs apache
 apache2-foreground
